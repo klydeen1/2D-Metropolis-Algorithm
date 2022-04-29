54b3658d4dbfd18a2889d07dd5846428ac608ce9
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-04-29 15:43:48 -0500

    Initial Commit

---
+++ Shared/_D_Metropolis_AlgorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_Metropolis_AlgorithmApp.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 4/29/22.
+//
+
+import SwiftUI
+
+@main
+struct _D_Metropolis_AlgorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
